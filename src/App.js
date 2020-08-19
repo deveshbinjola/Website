2d36a509edd2cd861eb115de77e3b736ee317d0c
Sunny Binjola <sunny@Sunnys-MacBook-Pro.local>
2020-08-19 10:23:48 -0600

    deploy

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import ReactDOM from "react-dom";
+import { BrowserRouter, Redirect, Switch } from "react-router-dom";
+import React, { Component } from 'react';
+import { HashRouter, Route, Link } from "react-router-dom";
+// styles
+import "assets/css/bootstrap.min.css";
+import "assets/scss/paper-kit.scss?v=1.2.0";
+import "assets/demo/demo.css?v=1.2.0";
+// pages
+import Index from "views/Index.js";
+import About from "views/About.js";
+import Blog from "views/Blog.js";
+import CreditAdvice from "views/CreditAdvice.js";
+import DigitalNomad from "views/DigitalNomad.js";
+import TravelBlog from "views/TravelBlog.js";
+import TravelTips from "views/TravelTips";
+import Hiking from "views/Hiking";
+import Iceland from "views/blog/Iceland";
+import Switzerland from "views/blog/Switzerland";
+import Paris from "views/blog/Paris";
+import ReactGA from "react-ga";
+import FirstCard from "views/FirstCard";
+import Top10Cards from "views/Top10Cards";
+import BattleoftheBest from "views/BattleoftheBest";
+import TravelSmarter from "views/blog/TravelSmarter";
+// others
+
+ReactGA.initialize('UA-82402078-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter basename="/">
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+
+          <hr />
+  
+          <Route exact path="/" component={Index} />
+          <Route path="/about" component={About} />
+        </div>
+      </HashRouter>
+    );
+  }
+}
+
+const Home = () => <div><h2>Home</h2></div>
+const About = () => <div><h2>About</h2></div>
+
+export default App;
+
